11f8c7bb541be4c7a4d3f016c54565a88e662517
1s22s1 <1s22s1@example.com>
2023-08-25 05:18:59 +0900

    Add empty string test

--- spec/string_extension_spec.rb
+++ spec/string_extension_spec.rb
@@ -3,9 +3,8 @@ require_relative 'spec_helper'
 using StringExtension
 
 RSpec.describe String do
-  subject { string.social_distance }
-
   describe '#social_distance' do
+    it { expect(''.social_distance).to eq '' }
     it { expect('abc'.social_distance).to eq 'a b c' }
     it { expect('あいう'.social_distance).to eq 'あ い う' }
   end
